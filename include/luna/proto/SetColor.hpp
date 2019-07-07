17e67635e17f845539a7c90e5bf1cd8c3e901025
Roman Chomik <romanchom@gmail.com>
2019-07-07 13:43:15 +0200

    Simplified SetColor command.

--- include/luna/proto/SetColor.hpp
+++ include/luna/proto/SetColor.hpp
@@ -7,13 +7,9 @@
 namespace luna {
 namespace proto {
 
-struct RGB {
-    Scalar<uint8_t> r, g, b;
-};
-
 struct StrandData {
     Scalar<uint8_t> id;
-    Union<Array<RGB>, Array<Scalar<uint16_t>>> data;
+    Array<std::byte> rawBytes;
 };
 
 struct SetColor
