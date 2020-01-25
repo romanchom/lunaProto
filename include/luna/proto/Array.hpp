8e73bda37d31625b9b0f47b8b7b40929fae05989
Roman Chomik <romanchom@gmail.com>
2020-01-25 18:20:32 +0100

    Added missing include in Array.hpp

--- include/luna/proto/Array.hpp
+++ include/luna/proto/Array.hpp
@@ -3,67 +3,64 @@
 #include "Pointer.hpp"
 #include "Scalar.hpp"
 
-namespace luna {
-namespace proto {
+#include <cstddef>
 
-template<typename T> 
-class Array
-{
-public:
-    void set(T* data, size_t size)
+namespace luna::proto {
+    template<typename T> 
+    struct Array
     {
-        mSize = size;
-        mData = data;
-    }
+        void set(T* data, size_t size)
+        {
+            mSize = size;
+            mData = data;
+        }
 
-    T& operator[](size_t index)
-    {
-        return data()[index]; 
-    }
+        T& operator[](size_t index)
+        {
+            return data()[index]; 
+        }
 
-    T const& operator[](size_t index) const
-    {
-        return data()[index]; 
-    }
+        T const& operator[](size_t index) const
+        {
+            return data()[index]; 
+        }
 
-    T* data() 
-    {
-        return mData;
-    }
+        T* data() 
+        {
+            return mData;
+        }
 
-    T const* data() const 
-    {
-        return mData;
-    }
+        T const* data() const 
+        {
+            return mData;
+        }
 
-    size_t size() const
-    {
-        return mSize;
-    }
+        size_t size() const
+        {
+            return mSize;
+        }
 
-    T* begin()
-    {
-        return data();
-    }
+        T* begin()
+        {
+            return data();
+        }
 
-    T const* begin() const
-    {
-        return data();
-    }
+        T const* begin() const
+        {
+            return data();
+        }
 
-    T* end()
-    {
-        return data() + size();
-    }
+        T* end()
+        {
+            return data() + size();
+        }
 
-    T const* end() const
-    {
-        return data() + size();
-    }
-private:
-    Scalar<uint16_t> mSize;
-    Pointer<T> mData;
-};
-
-}
+        T const* end() const
+        {
+            return data() + size();
+        }
+    private:
+        Scalar<uint16_t> mSize;
+        Pointer<T> mData;
+    };
 }
